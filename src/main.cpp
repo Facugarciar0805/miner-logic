6e3d541715a04b01d51fd8f6df540f6482b4e472
Facugarciar0805 <facugarciar@gmail.com>
2026-03-26 09:45:36 -0300

    hay que separar en procesos
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,76 @@
 #include <Arduino.h>
+TaskHandle_t Miner_Task;
+TaskHandle_t Comunication_Task;
+QueueHandle_t queue;
+
+void comunicationCore0(void * parameter){
+  int value = 0;
+
+  while (true) {
+      value++;
+      xQueueSend(queue, &value, portMAX_DELAY);
+      Serial.println("Sent: " + String(value));
+      vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
+void minerCore1(void * parameter){
+  int received;
+
+  while (true) {
+      if (xQueueReceive(queue, &received, portMAX_DELAY)) {
+          Serial.println("Received: " + String(received));
+      }
+  }
+}
+
+void setup(){
+  Serial.begin(115200);
+  
+  queue = xQueueCreate(5, sizeof(int));
+
+  xTaskCreatePinnedToCore(
+    comunicationCore0,
+    "TaskCore0",
+    10000,
+    NULL,
+    1,
+    &Comunication_Task,
+    0
+  );
+
+  xTaskCreatePinnedToCore(
+    minerCore1,
+    "TaskCore1",
+    10000,
+    NULL,
+    1,
+    &Miner_Task,
+    1
+  );
+
+
+}
+void loop(){
+
+}
+
+
+
+
+
+
+
+
+/*===================MNER LOGIC=====================*/
+
+
+/*#include <Arduino.h>
 #include <mbedtls/md.h>
+
+
+
+
+
 
 // --- Configuración Global ---
 String datos = "Bloque_Simulado_#106";
@@ -66,7 +137,7 @@ void loop() {
 /**
  * Implementación de SHA-256 usando la API genérica de mbedtls
  */
-String minarBloque(String datos, uint32_t nonce){
+/*String minarBloque(String datos, uint32_t nonce){
     byte shaResult[32];
     mbedtls_md_context_t ctx;
 
@@ -94,4 +165,88 @@ String minarBloque(String datos, uint32_t nonce){
         hashHex += buf;
     }
     return hashHex;
+}*/
+
+/*===================MQTT COMUNICATION=====================*/
+/*
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Configuración de red y broker
+const char* ssid = "UA-Alumnos";
+const char* password = "41umn05WLC";
+const char* mqtt_server = "100.54.236.155"; 
+int numero = 0; // Tu contador
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Conectando a ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi conectado");
 }
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Intentando conexión MQTT...");
+    if (client.connect("ESP32_Facundo_Client")) { // ID único
+      Serial.println("¡Conectado!");
+      client.subscribe("esp32/recibir");
+    } else {
+      Serial.print("falló, rc=");
+      Serial.print(client.state());
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Mensaje recibido [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  static unsigned long lastMsg = 0;
+  if (millis() - lastMsg > 500) { // Enviando cada 500ms
+    lastMsg = millis();
+    
+    // 1. Creamos un buffer para guardar el texto (ej: "Mensaje: 123")
+    char mensaje[20]; 
+    
+    // 2. Formateamos el texto y metemos el número adentro
+    snprintf(mensaje, 20, "Mensaje: %d", numero);
+    
+    // 3. Publicamos y sumamos al contador
+    client.publish("esp32/enviar", mensaje);
+    
+    Serial.print("Enviado: ");
+    Serial.println(mensaje);
+    
+    numero++; 
+  }
+} */
